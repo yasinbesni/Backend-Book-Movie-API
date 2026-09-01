4d39f578bbc9a25436ea842de359224cb67a6823
yasin BESNİ <yasinbesni@gmail.com>
2026-09-02 00:44:15 +0300

    updated

--- src/db/models/Movie.js
+++ src/db/models/Movie.js
@@ -3,17 +3,30 @@ import mongoose from "mongoose";
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: [
+      true,
+      "Film adı zorunludur.",
+    ],
   },
   releaseYear: {
     type: Number,
-    min: 1888,
+    min: [
+      1888,
+      "Film yılı 1888'den küçük olamaz.",
+    ],
   },
   voteAverage: {
     type: Number,
-    min: 0,
-    max: 10,
+    min: [
+      0,
+      "Film puanı 0'dan küçük olamaz.",
+    ],
+    max: [
+      10,
+      "Film puanı 10'dan büyük olamaz.",
+    ],
   },
 });
 
-export const Movie = mongoose.model("Movie", movieSchema);
+export const Movie =
+  mongoose.model("Movie", movieSchema);
