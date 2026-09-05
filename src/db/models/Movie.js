d2a56b548b78411bbb8b1d899a2eb27ba6578179
yasin BESNİ <yasinbesni@gmail.com>
2026-09-05 21:35:59 +0300

    updated

--- src/db/models/Movie.js
+++ src/db/models/Movie.js
@@ -1,32 +1,47 @@
 import mongoose from "mongoose";
 
-const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [
-      true,
-      "Film adı zorunludur.",
-    ],
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [
+        true,
+        "Film adı zorunludur.",
+      ],
+    },
+    releaseYear: {
+      type: Number,
+      min: [
+        1888,
+        "Film yılı 1888'den küçük olamaz.",
+      ],
+    },
+    voteAverage: {
+      type: Number,
+      min: [
+        0,
+        "Film puanı 0'dan küçük olamaz.",
+      ],
+      max: [
+        10,
+        "Film puanı 10'dan büyük olamaz.",
+      ],
+    },
   },
-  releaseYear: {
-    type: Number,
-    min: [
-      1888,
-      "Film yılı 1888'den küçük olamaz.",
-    ],
+  {
+    timestamps: true,
   },
-  voteAverage: {
-    type: Number,
-    min: [
-      0,
-      "Film puanı 0'dan küçük olamaz.",
-    ],
-    max: [
-      10,
-      "Film puanı 10'dan büyük olamaz.",
-    ],
+);
+
+movieSchema.index(
+  {
+    title: 1,
+    releaseYear: 1,
   },
-});
+  {
+    unique: true,
+  },
+);
 
 export const Movie =
   mongoose.model("Movie", movieSchema);
