db776ee098397b6803637ca080c473e3c0ebaff9
yasin BESNİ <yasinbesni@gmail.com>
2026-09-15 00:14:24 +0300

    52_Projection_select_lean_ile_Sorgu_Sonuclarini_Nasil_Sadelestiririz

--- movie-api/src/controllers/movies.js
+++ movie-api/src/controllers/movies.js
@@ -36,13 +36,19 @@ export const getMovies = async (req, res) => {
   const countQuery = Movie.countDocuments(filter);
 
   const moviesQuery = Movie.find(filter)
+    .select({
+      title: 1,
+      releaseYear: 1,
+      voteAverage: 1,
+    })
     .sort({
       voteAverage: -1,
       releaseYear: -1,
       _id: 1,
     })
     .skip(skip)
-    .limit(limit);
+    .limit(limit)
+    .lean();
 
   const totalItems = await countQuery.exec();
 
