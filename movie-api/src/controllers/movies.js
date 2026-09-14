59cab501de1d865b6fdc208eab4e26c041194e62
yasin BESNİ <yasinbesni@gmail.com>
2026-09-15 00:08:29 +0300

    48_Query_Parameters_req_query_ile_page_limit_Nasil_Alinir

--- movie-api/src/controllers/movies.js
+++ movie-api/src/controllers/movies.js
@@ -2,15 +2,31 @@ import mongoose from "mongoose";
 
 import { Movie } from "../db/models/movie.js";
 
+import {
+  parsePaginationParams,
+} from "../utils/parsePaginationParams.js";
+
 export const getMovies = async (req, res) => {
-  const movies = await Movie.find({}).sort({
-    voteAverage: -1,
-    releaseYear: -1,
-    _id: 1,
-  });
+  const {
+    page,
+    limit,
+  } = parsePaginationParams(req.query);
+
+  const skip = (page - 1) * limit;
+
+  const movies = await Movie.find({})
+    .sort({
+      voteAverage: -1,
+      releaseYear: -1,
+      _id: 1,
+    })
+    .skip(skip)
+    .limit(limit);
 
   res.status(200).json({
     message: "Filmler getirildi.",
+    page,
+    limit,
     data: movies,
   });
 };
