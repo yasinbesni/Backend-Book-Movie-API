1d5860448f1c8cbb549c02447d9065c570619b53
yasin BESNİ <yasinbesni@gmail.com>
2026-09-14 23:24:49 +0300

    39_Express_Merkezi_Hata_Yonetimi_Nasil_Kurulur

--- movie-api/src/server.js
+++ movie-api/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import { errorHandler }
+  from "./middlewares/errorHandler.js";
 import { requestLogger }
   from "./middlewares/requestLogger.js";
 import moviesRouter from "./routers/movies.js";
@@ -7,6 +9,7 @@ import moviesRouter from "./routers/movies.js";
 export const app = express();
 
 app.use(express.json());
+
 app.use(requestLogger);
 
 app.get("/", (req, res) => {
@@ -14,3 +17,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/movies", moviesRouter);
+
+app.use(errorHandler);
