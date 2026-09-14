1ca4870087d678f5c3143510e302f6d5fba664d5
yasin BESNİ <yasinbesni@gmail.com>
2026-09-14 23:04:25 +0300

    38_Express_Middleware_Proje_Mimarisi

--- movie-api/src/server.js
+++ movie-api/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 
+import { requestLogger }
+  from "./middlewares/requestLogger.js";
 import moviesRouter from "./routers/movies.js";
 
 export const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.get("/", (req, res) => {
   res.send("Merhaba Express!");
