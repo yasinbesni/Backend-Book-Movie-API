bc41a5f2fc6715221cdaf99729c7392e3479cdc2
yasin BESNİ <yasinbesni@gmail.com>
2026-09-14 21:13:49 +0300

    16_HTTP_Yöntemleriyle_Farklı_İşlemler.md

--- movie-api/index.js
+++ movie-api/index.js
@@ -10,6 +10,22 @@ app.get("/movies", (req, res) => {
   res.send("Filmler");
 });
 
+app.get("/movies/42", (req, res) => {
+  res.send("Film bilgileri");
+});
+
+app.put("/movies/42", (req, res) => {
+  res.send("Film tamamen güncellendi.");
+});
+
+app.patch("/movies/42", (req, res) => {
+  res.send("Filmin belirli alanları güncellendi.");
+});
+
+app.delete("/movies/42", (req, res) => {
+  res.send("Film silindi.");
+});
+
 app.listen(3000, () => {
   console.log("Server 3000 portunda çalışıyor.");
 });
